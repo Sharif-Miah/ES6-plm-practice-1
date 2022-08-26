f0f0ae4426265a883eb4e187057d0cda179b3d37
farhan sharif <farhansharif1410@gmail.com>
2022-08-26 12:03:37 -0230

    Two number plm solve

--- practice.js
+++ practice.js
@@ -17,6 +17,20 @@ const d = totalSum()
 
 // ২. টেম্পলেট স্ট্রিং দিয়ে একটা স্ট্রিং তৈরি করো। সেটার মধ্যে উপরে ডিক্লেয়ার করা ভেরিয়েবল এর মান ডাইনামিক ভাবে বসাও। একইভাবে উপরে একটা অবজেক্ট ডিক্লেয়ার করো। এবং ডাইনামিকভাবে উপরের অবজেক্ট এর দুইটা প্রপার্টি এর মান তোমার টেমপ্লেট স্ট্রিং এর মধ্যে বসাও।
 
+let nameIs = 'Fahan Sharif';
+const hobby = 'JavaScript Devoloper';
+
+const addres = {
+    home: 'Bonaid',
+    district: 'Narsingdi',
+    city: 'Dhaka',
+    country: 'Bangladesh'
+}
+
+const myInfo = `My name is ${nameIs}. I want to be a ${hobby}. I live in ${addres.home}. My district is ${addres.district}. My City is ${addres.city}. My country is ${addres.country}.`
+
+// console.log(myInfo);
+
 // ৩.১ একটা প্যারামিটার ওয়ালা arrow ফাংশন ডিক্লেয়ার করো। এবং সেই ফাংশনের কাজ হবে তুমি কোন ইনপুট দিলে সেই ইনপুট সংখ্যাকে ৫ দিয়ে ভাগ করে আউটপুট দিবে।
 
 // ৩.২ তুমি দুইটা প্যারামিটার ওয়ালা একটা অ্যারো ফাংশন লিখবে। সেই ফাংশনের ভিতরে কাজ হবে। প্রত্যেকটা ইনপুট প্যারামিটার এর সাথে ২ যোগ করবে তারপর যোগফল দুইটা গুণ করবে। ক্যামনে করবে সেটা চিন্তা করে বের করার চেষ্টা করো
